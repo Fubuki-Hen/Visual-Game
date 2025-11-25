2e4726db35a6a3653b45f896b6cf388a1789de5b
Fubuki-Hen <proihsan86@gmail.com>
2025-11-25 15:39:37 +0700

    Visual Game
    Brand Game Visual Game dari Gweh

---
+++ A.js
@@ -0,0 +1,43 @@
+document.getElementById("vidio1").addEventListener("mouseover", function() {
+            this.play();
+    });
+    
+        document.getElementById("vidio1").addEventListener("mouseleave",function() {
+        this.pause();
+    });
+document.getElementById("vidio2").addEventListener("mouseover", function() {
+            this.play();
+    });
+    
+        document.getElementById("vidio2").addEventListener("mouseleave",function() {
+        this.pause();
+    });
+document.getElementById("vidio3").addEventListener("mouseover", function() {
+            this.play();
+    });
+    
+        document.getElementById("vidio3").addEventListener("mouseleave",function() {
+        this.pause();
+    });
+document.getElementById("vidio4").addEventListener("mouseover", function() {
+            this.play();
+    });
+    
+        document.getElementById("vidio4").addEventListener("mouseleave",function() {
+        this.pause();
+    });
+document.getElementById("vidio5").addEventListener("mouseover", function() {
+            this.play();
+    });
+    
+        document.getElementById("vidio5").addEventListener("mouseleave",function() {
+        this.pause();
+    });
+document.getElementById("vidio6").addEventListener("mouseover", function() {
+            this.play();
+    });
+    
+        document.getElementById("vidio6").addEventListener("mouseleave",function() {
+        this.pause();
+    });
+    
